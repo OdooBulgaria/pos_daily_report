ceac7152881a7758c26f557c244b683c5e32b822
Gaurav Sahu <administrator@ho41.jayanita.jayanita.net>
2016-02-17 17:21:52 +0530

    Add Date Range

--- wizard/daily_pos_sale_wiz.py
+++ wizard/daily_pos_sale_wiz.py
@@ -5,6 +5,7 @@ from dateutil.relativedelta import relativedelta
 from tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
 import base64
 import tempfile
+import math
 
 class daily_pos_sale_wiz_view(osv.osv_memory):
 	_name = 'daily.pos.sale.wiz.view'
@@ -34,23 +35,40 @@ class binary_sale_report_text_file_wizard(osv.osv_memory):
     def _generate_sale_report_file(self, cr, uid, context=None):
         if context is None:
             context = {}
-        # employee_obj = self.pool.get('hr.employee')
-        # payslip_obj = self.pool.get('hr.payslip')
-        # period_data = self.pool.get('account.period').browse(cr, uid, context.get('datas')['period_id'][0], context=context)
-        # start_date = period_data.date_start
-        # end_date = period_data.date_stop
-        # emp_ids = employee_obj.search(cr, uid, [('bank_account_id', '!=', False)], order="name", context = context)
-        # print"emp_ids::::::::",emp_ids,start_date,end_date
-        # payslip_ids = payslip_obj.search(cr, uid, [('employee_id', 'in', emp_ids), ('cheque_number','=',False), ('date_from', '>=', start_date), ('date_from', '<=', end_date), ('state', 'in', ['draft', 'done', 'verify'])], order="employee_name")
-        # print"payslip_ids::::::::::",payslip_ids
-        # if not payslip_ids:
-        #     raise osv.except_osv(_('Error'), _('There is no payslip found to generate text file.'))
+        print context
+        print context.get('datas')
+        DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+        pos_order_obj = self.pool.get('pos.order')
+        start_date = context.get('datas')['date_from']
+        end_date = context.get('datas')['date_to']
+
+        start_date = datetime.strptime(start_date, DATETIME_FORMAT)
+        end_date = datetime.strptime(end_date, DATETIME_FORMAT)
+
+        from_date = datetime.strptime((start_date.strftime('%Y-%m-%d')),"%Y-%m-%d").date()
+        to_date = datetime.strptime((end_date.strftime('%Y-%m-%d')),"%Y-%m-%d").date()
+        
+        time_diff = to_date - from_date
+        diff_day = time_diff.days + (float(time_diff.seconds) / 86400)
+        diff_day = round(math.ceil(diff_day))
+
         tgz_tmp_filename = tempfile.mktemp('.' + "txt")
         tmp_file = False
         try:
             tmp_file = open(tgz_tmp_filename, "wr")
             net_amount_total=0.0
-            detail_record = 'Deatil Report sdfhsjfhsdjfhj'
+            detail_record = ''
+            next_date = from_date
+            for day in range(0, int(diff_day)):
+                detail_record = detail_record  + str(next_date) + '\n'
+                next_hr = next_date
+                time =   datetime.strftime(next_date,'%Y-%m-%d %H:%M:%S')
+                time = datetime.strptime(time,'%Y-%m-%d %H:%M:%S')
+                for hour in range(0,24):
+                    print "time",time
+                    detail_record = detail_record + '\t' + str(time) + '\n'
+                    time += timedelta(hours=1)
+                next_date +=  timedelta(days=1)
             
             # tmp_file.write(header_record)
             tmp_file.write(detail_record)
